de9479d6007f2eae2b3e9f0cafbed7453b23211d
Gilyazov Arthur <arik2517@gmail.com>
2024-03-05 12:35:20 +0700

    Add check pull request

--- tm-backend/src/main/java/org/nsu/fit/tm_backend/service/impl/PlanServiceImpl.java
+++ tm-backend/src/main/java/org/nsu/fit/tm_backend/service/impl/PlanServiceImpl.java
@@ -36,7 +36,7 @@ public class PlanServiceImpl implements PlanService {
      */
     public List<PlanPojo> getPlans(UUID customerId) {
         Boolean isNeed = true;
-        if(isNeed && true){
+        if(isNeed && true || false){
 
         List<UUID> usedPlanIds = customerId == null
                 ? Collections.emptyList()
